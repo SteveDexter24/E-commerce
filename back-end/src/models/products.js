b56dedf34a035f12e1024c9c0fe309a49992451e
SteveDexter24 <tamangsteve@gmail.com>
2021-08-16 00:24:21 +0800

    updated product model

--- back-end/src/models/products.js
+++ back-end/src/models/products.js
@@ -1,87 +1,94 @@
-const mongoose = require('mongoose')
-const helper = require('./helper/productHelper')
+const mongoose = require("mongoose");
+const helper = require("./helper/productHelper");
 
 const productSchema = new mongoose.Schema({
-  productName: {
-    en: helper.productNameObj,
-    cn: helper.productNameObj,
-    jpn: helper.productNameObj,
-  },
-  category: {
-    en: helper.categoryObj,
-    cn: helper.categoryObj,
-    jpn: helper.categoryObj,
-  },
-  gender: {
-    type: String,
-    required: true,
-  },
-  image: [
-    {
-      type: String,
-      required: true,
-      default: 'https://semantic-ui.com/images/wireframe/image.png',
+    productName: {
+        en: helper.productNameObj,
+        cn: helper.productNameObj,
+        jpn: helper.productNameObj,
     },
-  ],
-  feature: {
-    en: helper.featureObj,
-    cn: helper.featureObj,
-    jpn: helper.featureObj,
-  },
-  description: {
-    en: helper.descriptionObj,
-    cn: helper.descriptionObj,
-    jpn: helper.descriptionObj,
-  },
-  style: {
-    en: helper.styleObject,
-    cn: helper.styleObject,
-    jpn: helper.styleObject,
-  },
-  color: [
-    {
-      en: helper.colorObject,
-      cn: helper.colorObject,
-      jpn: helper.colorObject,
+    category: {
+        en: helper.categoryObj,
+        cn: helper.categoryObj,
+        jpn: helper.categoryObj,
     },
-  ],
-  price: {
-    hkd: helper.priceObject,
-    jpn: helper.priceObject,
-  },
-  ratings: {
-    type: Number,
-    default: 0,
-  },
-  material: {
-    type: String,
-  },
-  washing_care: {
-    type: String,
-  },
-  discount: {
-    hkd: helper.discountObject,
-    jpn: helper.discountObject,
-  },
-  size: [
-    {
-      sizeType: {
+    gender: {
         type: String,
         required: true,
-      },
-      sizeRemaining: {
-        type: Number,
-        required: true,
-        default: 0,
-      },
     },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    required: true,
-  },
-})
+    image: [
+        {
+            type: String,
+            required: true,
+            default: "https://semantic-ui.com/images/wireframe/image.png",
+        },
+    ],
+    feature: {
+        en: helper.featureObj,
+        cn: helper.featureObj,
+        jpn: helper.featureObj,
+    },
+    description: {
+        en: helper.descriptionObj,
+        cn: helper.descriptionObj,
+        jpn: helper.descriptionObj,
+    },
+    style: {
+        en: helper.styleObject,
+        cn: helper.styleObject,
+        jpn: helper.styleObject,
+    },
 
-const Product = mongoose.model('products', productSchema)
-module.exports = { productSchema, Product }
+    price: {
+        hkd: helper.priceObject,
+        jpn: helper.priceObject,
+    },
+    ratings: {
+        type: Number,
+        default: 0,
+    },
+    material: {
+        type: String,
+    },
+    washing_care: {
+        type: String,
+    },
+    discount: {
+        hkd: helper.discountObject,
+        jpn: helper.discountObject,
+    },
+    // XS : {colors: [{color: "blue", count: 6 }, {color: "red", count 8}]}
+
+    size: [
+        {
+            sizeType: {
+                type: String,
+                required: true,
+            },
+            color_size: {
+                colors: [
+                    {
+                        color: {
+                            en: helper.colorObject,
+                            cn: helper.colorObject,
+                            jpn: helper.colorObject,
+                        },
+                        count: {
+                            type: Number,
+                            required: true,
+                        },
+                    },
+                ],
+            },
+        },
+    ],
+
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        required: true,
+    },
+});
+
+const Product = mongoose.model("products", productSchema);
+module.exports = { productSchema, Product };
