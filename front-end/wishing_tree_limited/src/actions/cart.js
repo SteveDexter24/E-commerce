89ab21a0cd5f49ed369282ef6fec40474045791d
SteveDexter24 <tamangsteve@gmail.com>
2021-08-24 05:23:26 +0800

    cart route and controller done, next step: frontend refactor

--- front-end/wishing_tree_limited/src/actions/cart.js
+++ front-end/wishing_tree_limited/src/actions/cart.js
@@ -1,132 +1,177 @@
-import product from '../apis/api'
+import product from "../apis/api";
 import {
-  CART_ADD_ITEM,
-  UPDATE_CART_ITEM,
-  CART_REMOVE_ITEM,
-  CART_SAVE_SHIPPING_ADDRESS,
-  CART_SAVE_PAYMENT_METHOD,
-  CART_SAVE_USER_SHIPPING_INFO,
-} from './types'
+    CART_ADD_ITEM,
+    UPDATE_CART_ITEM,
+    CART_REMOVE_ITEM,
+    CART_SAVE_SHIPPING_ADDRESS,
+    CART_SAVE_PAYMENT_METHOD,
+    CART_SAVE_USER_SHIPPING_INFO,
+} from "./types";
 
-export const addToCart = (productId, qty, sizeType, color) => async (
-  dispatch,
-  getState,
-) => {
-  const { data } = await product.get(`/product/${productId}`)
+export const addToCart =
+    (productId, qty, sizeType, color) => async (dispatch, getState) => {
+        const { data } = await product.get(`/product/${productId}`);
 
-  const getSize = data.size.filter((s) => s.sizeType === sizeType)
+        const getSize = data.size.filter((s) => s.sizeType === sizeType);
 
-  const colorRemaining = getSize[0].colors.filter(
-    (s) => s.color[getState().settings.language] === color,
-  )
-  dispatch({
-    type: CART_ADD_ITEM,
-    payload: {
-      productId: data._id,
-      name: data.productName[getState().settings.language],
-      description: data.description[getState().settings.language],
-      image: data.image,
-      price: data.price[getState().settings.currency],
-      size: sizeType,
-      qty: Number(qty),
-      color: color,
-      totalSize: colorRemaining ? colorRemaining[0].count : 1,
-    },
-  })
+        const colorRemaining = getSize[0].colors.filter(
+            (s) => s.color[getState().settings.language] === color
+        );
+        dispatch({
+            type: CART_ADD_ITEM,
+            payload: {
+                productId: data._id,
+                name: data.productName[getState().settings.language],
+                description: data.description[getState().settings.language],
+                image: data.image,
+                price: data.price[getState().settings.currency],
+                size: sizeType,
+                qty: Number(qty),
+                color: color,
+                totalSize: colorRemaining ? colorRemaining[0].count : 1,
+            },
+        });
 
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+        localStorage.setItem(
+            "cartItems",
+            JSON.stringify(getState().cart.cartItems)
+        );
 
-  // After setting the cart items, the send a post request to the server;
-  console.log(getState().cart.cartItems)
-  const { userInfo } = getState().userAuth
-  try {
-    const config = {
-      headers: {
-        Authorization: `Bearer ${userInfo.token}`,
-      },
-    }
-    const response = await product.post(
-      `/user/${userInfo._id}/cart`,
-      {
-        item: {
-          productId: data._id,
-          name: data.productName[getState().settings.language],
-          description: data.description[getState().settings.language],
-          image: data.image,
-          price: data.price[getState().settings.currency],
-          size: sizeType,
-          qty: Number(qty),
-          color: color,
-          totalSize: colorRemaining ? colorRemaining[0].count : 1,
-        },
-      },
-      config,
-    )
+        // After setting the cart items, the send a post request to the server;
+        const { userInfo } = getState().userAuth;
+        try {
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${userInfo.token}`,
+                },
+            };
+            const response = await product.post(
+                `/user/${userInfo._id}/cart`,
+                {
+                    item: {
+                        productId: data._id,
+                        name: data.productName[getState().settings.language],
+                        description:
+                            data.description[getState().settings.language],
+                        image: data.image,
+                        price: data.price[getState().settings.currency],
+                        size: sizeType,
+                        qty: Number(qty),
+                        color: color,
+                        totalSize: colorRemaining ? colorRemaining[0].count : 1,
+                    },
+                },
+                config
+            );
 
-    console.log(response.data)
-  } catch (error) {
-    console.log(error)
-  }
-}
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-export const updateCart = (
-  productId,
-  qty,
-  sizeType,
-  color,
-  cartIndex,
-  description,
-) => async (dispatch, getState) => {
-  dispatch({
-    type: UPDATE_CART_ITEM,
-    payload: {
-      productId: productId,
-      qty: Number(qty),
-      size: sizeType,
-      color: color,
-      cartIndex: cartIndex,
-      description: description,
-    },
-  })
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-  console.log(getState().cart.cartItems)
-}
+export const updateCart =
+    (productId, qty, sizeType, color, cartIndex, description) =>
+    async (dispatch, getState) => {
+        dispatch({
+            type: UPDATE_CART_ITEM,
+            payload: {
+                productId: productId,
+                qty: Number(qty),
+                size: sizeType,
+                color: color,
+                cartIndex: cartIndex,
+                description: description,
+            },
+        });
+        localStorage.setItem(
+            "cartItems",
+            JSON.stringify(getState().cart.cartItems)
+        );
+        console.log(getState().cart.cartItems);
+        const { userInfo } = getState().userAuth;
 
-export const removeItemInCart = (productId, size, color) => async (
-  dispatch,
-  getState,
-) => {
-  dispatch({
-    type: CART_REMOVE_ITEM,
-    payload: {
-      productId: productId,
-      size: size,
-      color: color,
-    },
-  })
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-}
+        try {
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${userInfo.token}`,
+                },
+            };
+            const { data } = await product.patch(
+                `/user/${userInfo._id}/update-cart`,
+                {
+                    productId: productId,
+                    size: sizeType,
+                    qty: Number(qty),
+                    color: color,
+                },
+
+                config
+            );
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+export const removeItemInCart =
+    (productId, size, color) => async (dispatch, getState) => {
+        dispatch({
+            type: CART_REMOVE_ITEM,
+            payload: {
+                productId: productId,
+                size: size,
+                color: color,
+            },
+        });
+        localStorage.setItem(
+            "cartItems",
+            JSON.stringify(getState().cart.cartItems)
+        );
+
+        const { userInfo } = getState().userAuth;
+
+        try {
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${userInfo.token}`,
+                },
+            };
+            const { data } = await product.patch(
+                `/user/${userInfo._id}/remove`,
+                {
+                    productId: productId,
+                    size: size,
+                    color: color,
+                },
+                config
+            );
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
 export const saveShippingAddress = (data) => async (dispatch) => {
-  dispatch({
-    type: CART_SAVE_SHIPPING_ADDRESS,
-    payload: data,
-  })
-  localStorage.setItem('shippingAddress', JSON.stringify(data))
-}
+    dispatch({
+        type: CART_SAVE_SHIPPING_ADDRESS,
+        payload: data,
+    });
+    localStorage.setItem("shippingAddress", JSON.stringify(data));
+};
 
 export const saveUserShippingInfo = (data) => async (dispatch) => {
-  dispatch({
-    type: CART_SAVE_USER_SHIPPING_INFO,
-    payload: data,
-  })
-  localStorage.setItem('userShippingInfo', JSON.stringify(data))
-}
+    dispatch({
+        type: CART_SAVE_USER_SHIPPING_INFO,
+        payload: data,
+    });
+    localStorage.setItem("userShippingInfo", JSON.stringify(data));
+};
 
 export const savePaymentMethod = (paymentMethod) => async (dispatch) => {
-  dispatch({
-    type: CART_SAVE_PAYMENT_METHOD,
-    payload: paymentMethod,
-  })
-  localStorage.setItem('paymentMethod', JSON.stringify(paymentMethod))
-}
+    dispatch({
+        type: CART_SAVE_PAYMENT_METHOD,
+        payload: paymentMethod,
+    });
+    localStorage.setItem("paymentMethod", JSON.stringify(paymentMethod));
+};
--- front-end/wishing_tree_limited/src/reducers/cartReducers.js
+++ front-end/wishing_tree_limited/src/reducers/cartReducers.js
@@ -1,87 +1,90 @@
 import {
-  CART_ADD_ITEM,
-  UPDATE_CART_ITEM,
-  CART_REMOVE_ITEM,
-  CART_SAVE_SHIPPING_ADDRESS,
-  CART_SAVE_PAYMENT_METHOD,
-  CART_SAVE_USER_SHIPPING_INFO,
-} from '../actions/types'
+    CART_ADD_ITEM,
+    UPDATE_CART_ITEM,
+    CART_REMOVE_ITEM,
+    CART_SAVE_SHIPPING_ADDRESS,
+    CART_SAVE_PAYMENT_METHOD,
+    CART_SAVE_USER_SHIPPING_INFO,
+} from "../actions/types";
 
 export const cartReducer = (
-  state = { cartItems: [], shippingAddress: {}, paymentMethod: '' },
-  action,
+    state = { cartItems: [], shippingAddress: {}, paymentMethod: "" },
+    action
 ) => {
-  switch (action.type) {
-    case CART_ADD_ITEM:
-      const item = action.payload
-      const existItem = state.cartItems.find(
-        (x) =>
-          x.productId === item.productId &&
-          x.size === item.size &&
-          x.color === item.color,
-      )
+    switch (action.type) {
+        case CART_ADD_ITEM:
+            const item = action.payload;
+            const existItem = state.cartItems.find(
+                (x) =>
+                    x.productId === item.productId &&
+                    x.size === item.size &&
+                    x.color === item.color
+            );
 
-      // If same productId and same size and same color, then append the quantity
-      // else just add as a seperate product
-      if (existItem) {
-        const existIndex = state.cartItems.findIndex(
-          (x) =>
-            x.productId === existItem.productId &&
-            x.size === existItem.size &&
-            x.color === existItem.color,
-        )
+            // If same productId and same size and same color, then append the quantity
+            // else just add as a seperate product
+            if (existItem) {
+                const existIndex = state.cartItems.findIndex(
+                    (x) =>
+                        x.productId === existItem.productId &&
+                        x.size === existItem.size &&
+                        x.color === existItem.color
+                );
 
-        let cartList = [...state.cartItems]
-        cartList[existIndex] = {
-          ...cartList[existIndex],
-          qty: Number(cartList[existIndex].qty) + Number(item.qty),
-        }
+                let cartList = [...state.cartItems];
+                cartList[existIndex] = {
+                    ...cartList[existIndex],
+                    qty: Number(cartList[existIndex].qty) + Number(item.qty),
+                };
 
-        return {
-          ...state,
-          cartItems: cartList,
-        }
-      } else {
-        return { ...state, cartItems: [...state.cartItems, item] }
-      }
-    case UPDATE_CART_ITEM:
-      const updateItem = action.payload
-      if (state.cartItems[updateItem.cartIndex]) {
-        let cartArr = [...state.cartItems]
-        cartArr[updateItem.cartIndex] = {
-          ...cartArr[updateItem.cartIndex],
-          qty: Number(updateItem.qty),
-        }
-        return { ...state, cartItems: cartArr }
-      } else {
-        // return { ...state, cartItems: [...state.cartItems] }
-        return { ...state }
-      }
-    case CART_REMOVE_ITEM:
-      const removeItem = action.payload
-      const removeIndex = state.cartItems.findIndex(
-        (x) =>
-          x.productId === removeItem.productId &&
-          x.size === removeItem.size &&
-          x.color === removeItem.color,
-      )
+                return {
+                    ...state,
+                    cartItems: cartList,
+                };
+            } else {
+                return { ...state, cartItems: [...state.cartItems, item] };
+            }
+        case UPDATE_CART_ITEM:
+            const updateItem = action.payload;
 
-      if (removeIndex !== -1) {
-        return {
-          ...state,
-          cartItems: state.cartItems.filter((_, i) => i !== removeIndex),
-        }
-      } else {
-        return { ...state }
-      }
+            if (state.cartItems[updateItem.cartIndex]) {
+                let cartArr = [...state.cartItems];
+                cartArr[updateItem.cartIndex] = {
+                    ...cartArr[updateItem.cartIndex],
+                    qty: Number(updateItem.qty),
+                };
+                return { ...state, cartItems: cartArr };
+            } else {
+                // return { ...state, cartItems: [...state.cartItems] }
+                return { ...state };
+            }
+        case CART_REMOVE_ITEM:
+            const removeItem = action.payload;
+            const removeIndex = state.cartItems.findIndex(
+                (x) =>
+                    x.productId === removeItem.productId &&
+                    x.size === removeItem.size &&
+                    x.color === removeItem.color
+            );
 
-    case CART_SAVE_SHIPPING_ADDRESS:
-      return { ...state, shippingAddress: action.payload }
-    case CART_SAVE_USER_SHIPPING_INFO:
-      return { ...state, userShippingInfo: action.payload }
-    case CART_SAVE_PAYMENT_METHOD:
-      return { ...state, paymentMethod: action.payload }
-    default:
-      return state
-  }
-}
+            if (removeIndex !== -1) {
+                return {
+                    ...state,
+                    cartItems: state.cartItems.filter(
+                        (_, i) => i !== removeIndex
+                    ),
+                };
+            } else {
+                return { ...state };
+            }
+
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return { ...state, shippingAddress: action.payload };
+        case CART_SAVE_USER_SHIPPING_INFO:
+            return { ...state, userShippingInfo: action.payload };
+        case CART_SAVE_PAYMENT_METHOD:
+            return { ...state, paymentMethod: action.payload };
+        default:
+            return state;
+    }
+};
