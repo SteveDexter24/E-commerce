d04931e57f790aa96001666e1d84178aa47fbf4b
SteveDexter24 <tamangsteve@gmail.com>
2021-08-24 02:21:31 +0800

    add to cart (refactor + move to server) done

--- back-end/src/middlewares/cartManipulations.js
+++ back-end/src/middlewares/cartManipulations.js
@@ -1,29 +1,50 @@
 const { User } = require('../models/user')
 
-const cartManipulation = (req, res, next) => {
+const cartManipulation = async (req, res, next) => {
   const userId = req.params.id
-  //console.log(userId)
-  const item = req.body.item
 
-  //   try {
+  // item = the new item to be updated
+  const { item } = req.body
+  //console.log(item)
 
-  //     const foundUser = await User.findById(userId);
+  try {
+    const foundUser = await User.findById(userId)
 
-  //     const
+    const cartItems = foundUser.cart
 
-  //     const existItem = cartItems.find(
-  //     (x) =>
-  //       x.productId === item.productId &&
-  //       x.size === item.size &&
-  //       x.color === item.color,
-  //   )
+    const existItem = cartItems.find(
+      (x) =>
+        x.productId.toString() === item.productId &&
+        x.size === item.size &&
+        x.color === item.color,
+    )
 
-  //   }catch(error){
+    if (existItem) {
+      const existIndex = cartItems.findIndex(
+        (x) =>
+          x.productId === existItem.productId &&
+          x.size === existItem.size &&
+          x.color === existItem.color,
+      )
 
-  //   }
+      //   console.log('old cart items')
+      //   console.log(cartItems)
 
-  // console.log(existItem)
-  next()
+      cartItems[existIndex].qty =
+        Number(cartItems[existIndex].qty) + Number(item.qty)
+
+      //   console.log('new cart items')
+      //   console.log(cartItems)
+      req.cartItems = cartItems
+      next()
+    } else {
+      cartItems.push(item)
+      req.cartItems = cartItems
+      next()
+    }
+  } catch (error) {
+    res.send({ message: error.message })
+  }
 }
 
 module.exports = cartManipulation
