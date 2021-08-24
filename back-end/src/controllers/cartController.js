6ea31b76db96deee2806204dc2241f40ecc074c3
SteveDexter24 <tamangsteve@gmail.com>
2021-08-24 16:38:29 +0800

    added DB support for cart

--- back-end/src/controllers/cartController.js
+++ back-end/src/controllers/cartController.js
@@ -1,93 +1,103 @@
-const { User } = require("../models/user");
+const { User } = require('../models/user')
 
 module.exports = {
-    async addToCart(req, res, next) {
-        const userId = req.params.id;
-        const cartItems = req.cartItems;
+  async addToCart(req, res, next) {
+    const userId = req.params.id
+    const cartItems = req.cartItems
 
-        try {
-            const foundUser = await User.findById(userId);
+    try {
+      const foundUser = await User.findById(userId)
 
-            foundUser.cart = cartItems;
+      foundUser.cart = cartItems
 
-            const cart = await foundUser.save();
+      const cart = await foundUser.save()
 
-            res.status(200).send(cart.cart);
-        } catch (error) {
-            res.status(400).send({ message: error.message });
-        }
-    },
-    async updateCart(req, res, next) {
-        const userId = req.params.id;
-        const { qty, productId, size, color } = req.body;
-        console.log(req.body);
+      res.status(200).send(cart.cart)
+    } catch (error) {
+      res.status(400).send({ message: error.message })
+    }
+  },
+  async updateCart(req, res, next) {
+    const userId = req.params.id
+    const { qty, productId, size, color } = req.body
 
-        try {
-            const foundUser = await User.findById(userId);
-            let cartItems = foundUser.cart;
+    try {
+      const foundUser = await User.findById(userId)
+      let cartItems = foundUser.cart
 
-            const productExist = cartItems.find(
-                (x) =>
-                    x.productId.toString() === productId &&
-                    x.size === size &&
-                    x.color === color
-            );
+      const productExist = cartItems.find(
+        (x) =>
+          x.productId.toString() === productId &&
+          x.size === size &&
+          x.color === color,
+      )
 
-            if (!productExist) {
-                throw new Error("Product does not exist");
-            }
-            const productIndex = cartItems.findIndex(
-                (x) =>
-                    x.productId === productExist.productId &&
-                    x.color === productExist.color &&
-                    x.size === productExist.size
-            );
+      if (!productExist) {
+        throw new Error('Product does not exist')
+      }
+      const productIndex = cartItems.findIndex(
+        (x) =>
+          x.productId === productExist.productId &&
+          x.color === productExist.color &&
+          x.size === productExist.size,
+      )
 
-            if (cartItems[productIndex]) {
-                console.log(productIndex, qty);
-                cartItems[productIndex].qty = Number(qty);
-            } else {
-                throw new Error();
-            }
+      if (cartItems[productIndex]) {
+        cartItems[productIndex].qty = Number(qty)
+      } else {
+        throw new Error()
+      }
 
-            foundUser.cart = cartItems;
+      foundUser.cart = cartItems
 
-            const cart = await foundUser.save();
-            res.status(200).send(cart.cart);
+      const cart = await foundUser.save()
+      res.status(200).send(cart.cart)
 
-            //cartItems
-        } catch (error) {
-            res.send({ message: error.message });
-        }
-    },
-    async deleteItemInCart(req, res, next) {
-        const userId = req.params.id;
+      //cartItems
+    } catch (error) {
+      res.send({ message: error.message })
+    }
+  },
+  async deleteItemInCart(req, res, next) {
+    const userId = req.params.id
 
-        const { productId, size, color } = req.body;
+    const { productId, size, color } = req.body
 
-        try {
-            const foundUser = await User.findById(userId);
+    try {
+      const foundUser = await User.findById(userId)
 
-            const cartItems = foundUser.cart;
+      const cartItems = foundUser.cart
 
-            const removeIndex = cartItems.findIndex(
-                (x) =>
-                    x.productId.toString() === productId &&
-                    x.size === size &&
-                    x.color === color
-            );
+      const removeIndex = cartItems.findIndex(
+        (x) =>
+          x.productId.toString() === productId &&
+          x.size === size &&
+          x.color === color,
+      )
 
-            if (removeIndex != -1) {
-                let newCart = cartItems.filter((_, i) => i != removeIndex);
+      if (removeIndex != -1) {
+        let newCart = cartItems.filter((_, i) => i != removeIndex)
 
-                foundUser.cart = newCart;
-                const updatedCart = await foundUser.save();
-                res.status(200).send(updatedCart.cart);
-            } else {
-                throw new Error("could not find the item to delete");
-            }
-        } catch (error) {
-            res.send({ message: error.message });
-        }
-    },
-};
+        foundUser.cart = newCart
+        const updatedCart = await foundUser.save()
+        res.status(200).send(updatedCart.cart)
+      } else {
+        throw new Error('could not find the item to delete')
+      }
+    } catch (error) {
+      res.send({ message: error.message })
+    }
+  },
+  async moveCartToDB(req, res, next) {
+    const userId = req.params.id
+    const cartItems = req.cartItems
+    try {
+      const foundUser = await User.findById(userId)
+      foundUser.cart = cartItems
+      const cart = await foundUser.save()
+      res.status(200).send(cart.cart)
+    } catch (error) {
+      res.status(400).send({ message: error.message })
+    }
+  },
+}
