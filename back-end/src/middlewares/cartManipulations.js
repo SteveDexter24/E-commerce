220a5524d85e4f8471bf706f83e19331ac174634
SteveDexter24 <tamangsteve@gmail.com>
2021-08-24 23:41:08 +0800

    stable version

--- back-end/src/middlewares/cartManipulations.js
+++ back-end/src/middlewares/cartManipulations.js
@@ -1,58 +1,56 @@
-const { User } = require('../models/user')
+const { User } = require("../models/user");
 
 module.exports = {
-  async addToCartMiddleware(req, res, next) {
-    const userId = req.params.id
+    async addToCartMiddleware(req, res, next) {
+        const userId = req.params.id;
 
-    // item = the new item to be updated
-    const { item } = req.body
-    //console.log(item)
+        // item = the new item to be updated
+        const { item } = req.body;
+        //console.log(item)
 
-    try {
-      const foundUser = await User.findById(userId)
+        try {
+            const foundUser = await User.findById(userId);
 
-      const cartItems = foundUser.cart
+            const cartItems = foundUser.cart;
 
-      const existItem = cartItems.find(
-        (x) =>
-          x.productId.toString() === item.productId &&
-          x.size === item.size &&
-          x.color === item.color,
-      )
+            const existItem = cartItems.find(
+                (x) =>
+                    x.productId.toString() === item.productId &&
+                    x.size === item.size &&
+                    x.color === item.color
+            );
 
-      if (existItem) {
-        const existIndex = cartItems.findIndex(
-          (x) =>
-            x.productId === existItem.productId &&
-            x.size === existItem.size &&
-            x.color === existItem.color,
-        )
+            if (existItem) {
+                const existIndex = cartItems.findIndex(
+                    (x) =>
+                        x.productId === existItem.productId &&
+                        x.size === existItem.size &&
+                        x.color === existItem.color
+                );
 
-        cartItems[existIndex].qty =
-          Number(cartItems[existIndex].qty) + Number(item.qty)
+                cartItems[existIndex].qty =
+                    Number(cartItems[existIndex].qty) + Number(item.qty);
 
-        req.cartItems = cartItems
-        next()
-      } else {
-        cartItems.push(item)
-        req.cartItems = cartItems
-        next()
-      }
-    } catch (error) {
-      res.send({ message: error.message })
-    }
-  },
-  async moveToCartDatabase(req, res, next) {
-    const userId = req.params.id
-    const { cartItems } = req.body
+                req.cartItems = cartItems;
+                next();
+            } else {
+                cartItems.push(item);
+                req.cartItems = cartItems;
+                next();
+            }
+        } catch (error) {
+            res.send({ message: error.message });
+        }
+    },
+    async moveToCartDatabase(req, res, next) {
+        const userId = req.params.id;
+        const { cartItems } = req.body;
 
-    try {
-      // To add items from previous cart
-
-      req.cartItems = cartItems
-      next()
-    } catch (error) {
-      res.send({ message: error.message })
-    }
-  },
-}
+        try {
+            req.cartItems = cartItems;
+            next();
+        } catch (error) {
+            res.send({ message: error.message });
+        }
+    },
+};
