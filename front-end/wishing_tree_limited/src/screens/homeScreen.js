7712e08151bba11684023523cae54ae11d2e01da
SteveDexter24 <tamangsteve@gmail.com>
2021-09-02 00:42:25 +0800

    change carousel to card for home page

--- front-end/wishing_tree_limited/src/screens/homeScreen.js
+++ front-end/wishing_tree_limited/src/screens/homeScreen.js
@@ -1,29 +1,53 @@
-import { style } from 'dom-helpers'
-import React from 'react'
-import { Row, Col, Image, Button, Carousel } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { style } from "dom-helpers";
+import React from "react";
+import { Row, Col, Image, Button, Carousel, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const CarouselComponent = ({ history }) => {
+    return (
+        <Carousel variant="dark" controls={false} indicators={false}>
+            <Carousel.Item>
+                <Image
+                    className="d-block w-100"
+                    height={450}
+                    style={{ objectFit: "cover" }}
+                    src="/images/hk.jpeg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <Button variant="success" rounded>
+                        <h5>New Arrivals</h5>
+                    </Button>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+};
 
 const HomeScreen = ({ history }) => {
-  return (
-    <>
-      <Carousel variant="dark" controls={false} indicators={false}>
-        <Carousel.Item>
-          <Image
-            className="d-block w-100"
-            height={450}
-            style={{ objectFit: 'cover' }}
-            src="/images/hk.jpeg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <Button variant="success" rounded>
-              <h5>New Arrivals</h5>
-            </Button>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </>
-  )
-}
+    return (
+        <>
+            <Card className="bg-dark text-white">
+                <Card.Img
+                    src="/images/hk.jpeg"
+                    alt="Image"
+                    height={450}
+                    style={{ objectFit: "cover" }}
+                />
+                <Card.ImgOverlay>
+                    <Card.Title as="h1" style={{ fontSize: "2rem" }}>
+                        Wishing Tree Limited
+                    </Card.Title>
+                    <Card.Text className="d-flex justify-content-center">
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                    </Card.Text>
+                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                </Card.ImgOverlay>
+            </Card>
+        </>
+    );
+};
 
-export default HomeScreen
+export default HomeScreen;
--- front-end/wishing_tree_limited/src/screens/searchScreen.js
+++ front-end/wishing_tree_limited/src/screens/searchScreen.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
-import SearchBox from '../components/searchBox'
+import React from "react";
+import { Route } from "react-router-dom";
+import SearchBox from "../components/searchBox";
 
 const SearchScreen = () => {
-  return (
-    <div style={{ position: 'relative', top: '3rem' }}>
-      <Route
-        render={({ history }) => (
-          <SearchBox
-            history={history}
-            placeholder="Search products by name, style or category"
-          />
-        )}
-      />
-    </div>
-  )
-}
+    return (
+        <>
+            <h1>Search Products</h1>
 
-export default SearchScreen
+            <Route
+                render={({ history }) => (
+                    <SearchBox
+                        history={history}
+                        placeholder="Search products by name, style or category"
+                    />
+                )}
+            />
+        </>
+    );
+};
+
+export default SearchScreen;
